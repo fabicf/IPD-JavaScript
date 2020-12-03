eb1bb12f489a9f0d9d557e85f8fa16298003d663
fabicf <fabiolacezarfaria@gmail.com>
2020-12-03 09:05:47 -0500

    Day01 class notes

---
+++ Day01-notes/script.js
@@ -0,0 +1,29 @@
+//alert("Hey! JS this side");
+//console.log("Just trying console log");
+
+var foo = 10;
+var bar = 7;
+var sum = foo + bar;
+
+console.log("The sum is " + sum + " in a older way");
+
+console.log(`The sum is ${sum} in a cooler way`);
+console.log(`The sum is ${foo + bar} in a cooler way`);
+
+if (foo + bar > 15) {
+  console.log("yaaaay");
+} else {
+  console.log("noooooooo");
+}
+
+/*data types
+
+boolean
+null
+undefined
+numbers
+big int
+string
+symbol
+
+*/
